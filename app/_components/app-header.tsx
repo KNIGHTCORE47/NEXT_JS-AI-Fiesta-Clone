768dc22b5115cb0262c6c12fd28b5e648bf8e658
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2025-09-27 21:49:21 +0530

    feat(header): implement Clerk-based auth buttons with conditional rendering
    - Replaced static sign-in button with dynamic Clerk integration using `SignInButton` and `SignOutButton`
    - Leveraged `useUser` to conditionally render auth controls based on session state
    - Enabled modal-based sign-in and redirect-to-home on sign-out for smoother UX
    
    This update enhances the header with real-time authentication awareness and seamless user flow.

--- app/_components/app-header.tsx
+++ app/_components/app-header.tsx
@@ -2,20 +2,43 @@
 
 import { Button } from '@/components/ui/button';
 import { SidebarTrigger } from '@/components/ui/sidebar';
-import React from 'react'
+import {
+    SignInButton,
+    SignOutButton,
+    useUser
+} from '@clerk/nextjs';
+import React from 'react';
+
 
 export default function AppHeader() {
+    const authUser = useUser();
+
+
     return (
         <div
             className='p-4 w-full flex items-center justify-between shadow-md dark:shadow-secondary-foreground/20'
         >
             <SidebarTrigger />
 
-            <Button
-                variant='default'
-            >
-                Sign-In
-            </Button>
+            {
+                authUser.isLoaded && authUser.isSignedIn ? (
+                    <SignOutButton
+                        redirectUrl='/'
+                    >
+                        <Button variant={"default"}>
+                            Sign-Out
+                        </Button>
+                    </SignOutButton>
+                ) : (
+                    <SignInButton
+                        mode="modal"
+                    >
+                        <Button variant={"default"}>
+                            Sign-In/Sign-Up
+                        </Button>
+                    </SignInButton>
+                )
+            }
         </div>
     )
 }
